6732de375893949170bc93bb6485ad07a3c49547
knolchie <1810653932tud.fh-kufstein.ac.at>
2018-10-16 20:58:02 +0200

    otto 2

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+public class Main {
+
+    public static void main(String[] args)
+
+
+    {
+
+        int x, y, z, a;
+        z=1;
+        x = 9;
+        y = 5;
+        a = z + x + y;
+
+
+        System.out.println( x);
+
+        System.out.println( y);
+
+        System.out.println( z);
+
+        System.out.println( a);
+
+
+
+    }
+}
